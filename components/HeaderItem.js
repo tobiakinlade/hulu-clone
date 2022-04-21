e8ce44665686a275ee5c3c87bc15fc95b4ef8425
tobiakinlade <tobi.akinlade@yahoo.com>
2022-04-21 13:16:24 +0100

    initial commit

---
+++ components/HeaderItem.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function HeaderItem({ title, Icon }) {
+  return (
+    <div className='flex flex-col cursor-pointer w-12 sm:w-20 hover:text-white items-center group'>
+      <Icon className='h-8 mb-1 group-hover:animate-bounce' />
+      <p className='opacity-0 group-hover:opacity-100 hover:opacity-100 tracking-widest'>
+        {title}
+      </p>
+    </div>
+  );
+}
+
+export default HeaderItem;
